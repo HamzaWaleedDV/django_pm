517cd654e85bc6f036335f35f57a930169a7a354
Hamza Waleed <hamzawaleed.grant@gmail.com>
2023-07-27 15:12:46 +0300

    remove debug toolbar

--- projects_managment/settings.py
+++ projects_managment/settings.py
@@ -147,8 +147,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    # ...
-]
+# INTERNAL_IPS = [
+#     # ...
+#     "127.0.0.1",
+#     # ...
+# ]
